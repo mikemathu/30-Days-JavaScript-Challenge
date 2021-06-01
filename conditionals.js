fd769366283e61f047e9e411328f6acb00ee8ace
Michael Mathu <mathumichael17@gmail.com>
2021-06-01 07:49:21 +0300

    day 4 excercise one

---
+++ conditionals.js
@@ -0,0 +1,80 @@
+// let userAge = prompt('Enter Your Age')
+
+// if(userAge >= 18) {
+//  console.log('You are old enough to drive')
+// } else {
+//     console.log ('wait for the number of years he neds to turn 18')
+// }
+
+// console.log(userName)
+
+/* IF..ELSE */
+// let userAge = prompt('Enter Your Age')
+// let myAge = 22
+
+// if (userAge > myAge ){
+//     console.log(`Your are ${userAge - 22} years older tham me` )
+// } else if (userAge < myAge){
+//     console.log(`You are ${22 - userAge} years younger than me`)
+// }
+
+// TERNARY OPERATOR
+let a = 4
+let b = 3
+
+// If..else 
+// if(a > b){
+//     console.log('a is greater than b')
+// } else {
+//     console.log('a is less than b')
+// }
+
+// Ternary operator
+// a > b 
+//     ? console.log('You need a rain coat.')
+//     : console.log('No need for a rai coat.')
+
+    /* CHEKING FOR EVEN  NUMBERS */
+    // let number = 8
+
+    // if (number % 2 == 0) {
+    //     console.log(`${number} is an even number`)
+    // } else {
+    //     console.log(`${number} is not even`)
+    // }
+
+    // Ternary Operator
+    // number % 2 == 0 
+    //     // ? console.log(`${number} is an even number`)
+    //     ? document.write(`${number} is an even number`)
+    //     : console.log(`${number} is not even`)
+
+    /*Exercise Level 2 */
+    // STUDENT GRADES
+    let studentScore = 30
+
+    switch (studentScore) {
+        case studentScore > 80:
+            console.log('A')
+            break
+        case 70-79:
+            console.log('B')
+            break
+        case 60-69:
+            console.log('C')
+            break
+        case 50-59:
+            console.log('D')
+            break
+        case studentScore < 50:
+            console.log('F')
+            break
+        default:
+            console.log('Okay')
+    }
+
+    // SEASONS
+    
+
+
+        
