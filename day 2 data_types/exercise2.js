194e556c6acafe1457dcb07294c63716f216b420
Michael Mathu <mathumichael17@gmail.com>
2021-06-01 05:30:25 +0300

    finalized day two challenge

--- day 2 data_types/exercise2.js
+++ day 2 data_types/exercise2.js
@@ -1,3 +1,33 @@
-let quote = 'There is no exercise better for the heart than reaching down and lifting people up.'
+let quote = `'There is no exercise better for the heart than reaching down and lifting people up.'`
+let motherTeresaQuote = `"Love is not patronizing and charity isn't about pity, it is about love. Charity and love are the same -- with charity you give love, so don't just give money but reach out your hand instead."`
 
-console.log('The quote ' + quote + ' by John Holmes teaches us to help one another')
+let love = 'Love is the best thing in this world. Some found their love and some are still looking for their love.' 
+let numberTen = '10'
+let num = 9.81
+let numFloat = Number(num)
+
+
+// console.log('The quote ' + quote + ' by John Holmes teaches us to help one another')
+// console.log(motherTeresaQuote)
+
+// console.log(love.count('love'))
+// console.log(typeof(numberTen))
+
+console.log(numFloat)
+
+
+
+
+
+
+
+
+
+
+
+
+
+// UNSOLVED
+// typeof '10' is exactly equal to 10.
+
+
