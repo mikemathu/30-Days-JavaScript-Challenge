c2b54b623ebfffb6b60bc64fc19665301c78a3b8
Michael Mathu <mathumichael17@gmail.com>
2021-06-05 13:43:03 +0300

    normal functions and arrow function

---
+++ Day 7 -Functions/functions.js
@@ -0,0 +1,48 @@
+// function without parameter
+function square(){
+    let num = 2
+    let sq = num * num
+    // console.log(sq)
+}
+
+// square()
+
+function addTwoNumbers(){
+    let numOne = 10
+    let numTwo = 20
+    let sum = numOne + numTwo
+
+    // console.log(sum)
+}
+
+// addTwoNumbers()
+
+// Function Returning Value
+function printFullName (){
+    let firstName = 'Mike'
+    let lastName = 'Mathu'
+    let space = ' '
+    let fullName = firstName + space + lastName
+
+    return fullName
+}
+
+// console.log(printFullName())
+
+// Function with a parameter
+function areaOfCircle(r){
+    let area = Math.PI * r * r
+    return area
+}
+
+// console.log(areaOfCircle(10))
+
+// Function with two Parameters
+function sumTwoNumbers(numOne, numTwo) {
+    let sum = numOne + numTwo
+    return sum
+}
+
+// console.log(sumTwoNumbers(3, 20))
+
+// Function with many Paremeters
