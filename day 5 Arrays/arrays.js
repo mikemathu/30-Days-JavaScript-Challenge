b95b77b90b1078909748a2f728f2c1c227c2e764
Michael Mathu <mathumichael17@gmail.com>
2021-06-06 09:33:09 +0300

    Arrays

---
+++ day 5 Arrays/arrays.js
@@ -0,0 +1,23 @@
+// Array constructor
+const arr = Array()
+
+// or
+
+let arr = new Array()
+
+// Using sqare brackets([]) the most recommended way to create an empty list
+
+const arr = []
+
+// Creating an array with values
+const numbers = [0, 3.14, 9.18, 37, 98.6, 100]
+
+
+// Array with different data types
+const arr = [
+    'Mike',
+    250,
+    true,
+    {country: 'Kenya', city: 'Nairobi'},
+    {skills: ['HTML', 'CSS', 'JS', 'React', 'PHP', 'Laravel']}
+]
